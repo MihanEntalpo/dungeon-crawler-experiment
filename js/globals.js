7bc241930b4e0d965a7b270bb421d31b59b42e80
mihanentalpo <mihanentalpo@yandex.ru>
2026-02-09 11:47:59 +0700

    Правка глобальных переменных

--- js/globals.js
+++ js/globals.js
@@ -3,4 +3,4 @@
 // Глобальные словари ресурсов
 const all_images = Object.create(null);
 const all_tilesets = Object.create(null);
-const animations = Object.create(null);
+const all_animations = Object.create(null);
